413de47b09507070f77ebf31e5efcdc431821e8f
Gowthaman T <happygowthaman@gmail.com>
2021-07-28 12:08:27 +0530

    File Archietecture updated

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,24 @@ var uglify = require('gulp-uglify');
 var pipeline = require('readable-stream').pipeline;
 
 function compileSass() {
-  return gulp.src('src/styles/Sass/styles.scss')
+  return gulp.src('styles/Sass/styles.scss')
     .pipe(sass())
-    .pipe(gulp.dest('src/styles/Css'));
+    .pipe(gulp.dest('styles/Css'));
 }
 
 function watchChanges() {
-  gulp.watch('src/styles/Sass/**/*.scss', compileSass); 
+  gulp.watch('styles/Sass/**/*.scss', compileSass); 
 }
 
 function minifyCss() {
-  return gulp.src('src/styles/Css/styles.css')
+  return gulp.src('styles/Css/styles.css')
     .pipe(uglifycss())
     .pipe(gulp.dest('dist/'));
 }
 
 function minifyJs() {
   return pipeline(
-    gulp.src('src/js/*.js'),
+    gulp.src('js/*.js'),
     uglify(),
     gulp.dest('dist/')
 );
